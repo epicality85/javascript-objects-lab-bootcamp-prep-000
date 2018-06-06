e956946aa36a7e823366d0207558fe5e8b7b4ce2
Daniel O'Brien <dannyk85.dob@gmail.com>
2018-06-06 00:58:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 // defines a `recipes` object:
 var recipes = {}
-
+/*
 // returns an object with the original key value pairs and the new key value pair:
 function updateObjectWithKeyAndValue(object, key, value)
 {
   recipes[key] = value
   return recipes
-}
+}*/
 
 // it does not modify the original object, but rather returns a clone with the new data:
 function updateObjectWithKeyAndValue(object, key, value)
@@ -15,9 +15,9 @@ function updateObjectWithKeyAndValue(object, key, value)
   retObj[key] = value
   return retObj
 }
-
+/*
 // updates `object` with the given `key` and `value` (it is destructive) and returns theentire updated object:
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value)
 {
   
-}
+}*/
