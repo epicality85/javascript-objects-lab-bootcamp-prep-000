f502e713a6e026d5abcddce60e71462db0150c25
Daniel O'Brien <dannyk85.dob@gmail.com>
2018-06-06 00:50:52 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var recipes = {}
 // returns an object with the orignal key value pairs and the new key value pair:
 function updateObjectWithKeyAndValue(object, key, value)
 {
-  return recipes[key]
+  recipes[key] = value
 }
 
 // it does not modifythe original object, but rather returns a clone with the new data:
